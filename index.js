922c5197afdfd15bd07c81a77576d3398df0b714
Alexandre Dias <alexandrermd@gmail.com>
2020-02-04 00:16:37 -0300

    Encapsule more functions and write comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const readline = require('readline');
-const { performance } = require('perf_hooks');
-const { catchAll } = require('./pokemon.js');
+/* 
+    This part is necessary if we want track time performance.
+    const { performance } = require('perf_hooks');
+*/
+const { catchPokemons } = require('./pokemon.js');
 const INTERFACE = {
     input: process.stdin,
     output: process.stdout,
@@ -9,9 +12,19 @@ const INTERFACE = {
 
 const rl = readline.createInterface(INTERFACE);
 
-rl.on('line', async function (line) {
-    const v1 = performance.now();
-    const result = catchAll(line);
-    const v2 = performance.now();
-    console.log(`${result} in ${v2 - v1}ms`);
+/**
+ * Function to read file and compute movements
+ * @param line Line of file
+ */
+rl.on('line', function (line) {
+    /*
+        This part is necessary if we want track time performance.
+        const v1 = performance.now();
+        const result = catchPokemons(line);
+        const v2 = performance.now();
+        console.log(`${result} in ${v2 - v1}ms`);
+    */
+
+   const result = catchPokemons(line);
+   console.log(`${result}`);
 });
--- pokemon.js
+++ pokemon.js
@@ -1,49 +1,77 @@
-const catchAll = (string) => {
-    const caminho = { '0,0': '0,0' };
+/**
+ * Receive actual position and next step
+ * @param lat Latitude in world
+ * @param lon Longitude in world
+ * @param step What next step
+ * @return {Object} New position based in last position and movement
+ */
+const walkLatLon = (lat, lon, step) => {
+    lat = parseInt(lat, 10);
+    lon = parseInt(lon, 10);
 
-    const arrSteps = string.split('');
-    let actual = Object.keys(caminho)[0];
+    step = step.toUpperCase();
 
-    arrSteps.forEach(step => {
-        let [ lat, lon ] = actual.split(',');
+    switch (step) {
+        case 'N':
+            lat -= 1;
+        break;
 
-        lat = parseInt(lat, 10);
-        lon = parseInt(lon, 10);
+        case 'S':
+            lat += 1;
+        break;
+        
+        case 'E':
+            lon -= 1; 
+        break;
+        
+        case 'O':
+            lon += 1; 
+        break;
+        
+        default: 
+            return;
+    }
 
-        step = step.toUpperCase();
+    return { lat, lon };
+}
 
-        switch (step) {
-            case 'N':
-                lat -= 1;
-            break;
+/**
+ * Create and compute how many pokemon were caught
+ * @param str String with which movements trainer did
+ * @return {Integer} How many pokemons were caught
+ */
+const catchPokemons = (str) => {
+    const trail = { '0,0': 1 };
 
-            case 'S':
-                lat += 1;
-            break;
-            
-            case 'E':
-                lon -= 1; 
-            break;
-            
-            case 'O':
-                lon += 1; 
-            break;
-            
-            default: 
-                console.log('unknown');
-                return;
+    const steps = str.split('');
+    let actual = Object.keys(trail)[0];
+    let lat, lon;
+
+    if (steps.length === 1) {
+        return 2;
+    }
+
+    steps.forEach(step => {
+        [ lat, lon ] = actual.split(',');
+        const walk = walkLatLon(lat, lon, step);
+
+        if (walk) {
+            lat = walk.lat;
+            lon = walk.lon;
+        } else {
+            return;
         }
 
         actual = `${lat},${lon}`;
 
-        if (caminho[actual] === undefined) {
-            caminho[actual] = actual;
+        if (!trail[actual]) {
+            trail[actual] = 1;
         }
     });
 
-    return Object.keys(caminho).length;
+    return Object.keys(trail).length;
 }
 
 module.exports = {
-    catchAll,
+    catchPokemons,
 }
